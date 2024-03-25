2ce2258d531b5fe0c323ae13187e4b1fc651aa48
NeethuKPushkaran <141960227+NeethuKPushkaran@users.noreply.github.com>
2024-03-25 09:31:25 +0530

    'first-commit'

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+class Program
+{
+    public  void Add(double X)
+    {
+        X++;
+        Console.WriteLine($"inside value: {X}");
+
+    }
+
+    public static void Main()
+    {
+        double number = 5;
+        Program p1 = new Program();
+        p1.Add(number);
+        Console.WriteLine($"Outside Value: {number}");
+    }
+}
+
+
